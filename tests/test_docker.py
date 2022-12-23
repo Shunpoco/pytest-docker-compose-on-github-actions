1ae49093e234df43e7a924493c198cecbef514d5
Shunpoco <tkngsnsk313320@gmail.com>
2022-12-23 18:24:01 +0900

    Use PostgreSQL

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,17 +1,8 @@
 import docker
 import time
-import requests
 import pytest
-from requests.exceptions import ConnectionError
+import psycopg2
 
-
-def is_responsive(url):
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            return True
-    except ConnectionError:
-        return False
 
 def wait_until_responsive(func, pause: float, timeout: float = 30.0):
     start = time.time()
@@ -20,6 +11,15 @@ def wait_until_responsive(func, pause: float, timeout: float = 30.0):
             raise Exception
         time.sleep(pause)
 
+
+def check(container):
+    try:
+        logs = container.logs().decode("UTF-8")
+        if "database system is ready to accept connections" in logs:
+            return True
+    except Exception:
+        return False
+        
 
 @pytest.fixture
 def container():
@@ -30,28 +30,32 @@ def container():
     network = list(run_list[0].attrs["NetworkSettings"]["Networks"].keys())[0]
 
     container = client.containers.run(
-        "kennethreitz/httpbin:latest",
+        "postgres:13.9-bullseye",
         detach=True,
         network=network,
         name="tests_httpbin_1",
         remove=True,
+        environment={
+            "POSTGRES_PASSWORD": "admin",
+            "POSTGRES_PORT": "5432",
+        },
     )
 
     try:
-        wait_until_responsive(lambda: is_responsive("http://tests_httpbin_1"), 1.0)
+        wait_until_responsive(lambda: check(container), 1.0)
         yield container
     except Exception as e:
         print(e)
     finally:
-        print("Teardown...")
         container.stop()
 
 
-
 def test_status_code(container):
-    url = "http://tests_httpbin_1/status/200"
-
-    response = requests.get(url)
-
-    assert response.status_code == 200
-
+    with psycopg2.connect(
+            host="tests_httpbin_1",
+            user="postgres",
+            password="admin",
+    ) as db:
+        with db.cursor() as cur:
+            cur.execute("CREATE TABLE IF NOT EXISTS test (id TEXT PRIMARY KEY, name TEXT);")
+            db.commit()
