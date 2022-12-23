b2454202dd8c885d0a04ccd87673fc073c66b883
Shunpoco <tkngsnsk313320@gmail.com>
2022-12-23 13:44:40 +0900

    Clean up test

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,61 +1,57 @@
 import docker
 import time
 import requests
-# import pytest
-# import requests
-
-# from requests.exceptions import ConnectionError
+import pytest
+from requests.exceptions import ConnectionError
 
 
-# def is_responsive(url):
-#     try:
-#         response = requests.get(url)
-#         if response.status_code == 200:
-#             return True
+def is_responsive(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+    except ConnectionError:
+        return False
 
-#     except ConnectionError:
-#         return False
-
-
-# @pytest.fixture(scope="session")
-# def http_service(docker_ip, docker_services):
-#     port = docker_services.port_for("httpbin", 80)
-#     # url = "http://{}:{}".format(docker_ip, port)
-#     url = "http://tests_httpbin_1"
-#     docker_services.wait_until_responsive(
-#         timeout=30.0, pause=0.1, check=lambda: is_responsive(url),
-#     )
-
-#     return urld
-
-    
+def wait_until_responsive(func, pause: float, timeout: float = 30.0):
+    start = time.time()
+    while not func():
+        if time.time() - start >= timeout:
+            raise Exception
+        time.sleep(pause)
 
 
-# def test_status_code(http_service):
-def test_status_code():
+@pytest.fixture
+def container():
     client = docker.from_env()
     run_list = client.containers.list()
     if len(run_list) == 0:
-        print("There is no running image")
-        assert False
+        raise Exception
     network = list(run_list[0].attrs["NetworkSettings"]["Networks"].keys())[0]
-    print(network)
 
-    # Run a new container
-    container = client.containers.run("kennethreitz/httpbin:latest", detach=True, network=network, name="tests_httpbin_1", remove=True)
-    time.sleep(10)
-    print(container.logs())
+    container = client.containers.run(
+        "kennethreitz/httpbin:latest",
+        detach=True,
+        network=network,
+        name="tests_httpbin_1",
+        remove=True,
+    )
+
+    try:
+        wait_until_responsive(lambda: is_responsive("http://tests_httpbin_1"), 1.0)
+        yield container
+    except Exception as e:
+        print(e)
+    finally:
+        print("Teardown...")
+        container.stop()
+
+
+
+def test_status_code(container):
     url = "http://tests_httpbin_1/status/200"
 
     response = requests.get(url)
 
-    print(response.status_code)
-    print(response)
+    assert response.status_code == 200
 
-    container.stop()
-    
-    assert False
-    # status = 418
-    # response = requests.get(http_service + "/status/{}".format(status))
-
-    # assert response.status_code == status
