62fbfba4d1a06ef0afd97214315aa692a2a2eeeb
Shunpoco <tkngsnsk313320@gmail.com>
2022-12-26 17:18:08 +0900

    Modify

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,7 +1,15 @@
+import os
 import docker
 import time
 import pytest
 import psycopg2
+
+IS_LOCAL = os.getenv("IS_LOCAL", False)
+LOCAL_PORT = "5434"
+
+POSTGRES_HOST = "postgres"
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "admin"
 
 
 def wait_until_responsive(func, pause: float, timeout: float = 30.0):
@@ -24,24 +32,33 @@ def check(container):
 @pytest.fixture
 def container():
     client = docker.from_env()
-    run_list = client.containers.list()
-    if len(run_list) == 0:
-        raise Exception
-    network = list(run_list[0].attrs["NetworkSettings"]["Networks"].keys())[0]
+    options = {
+        "detach": True,
+        "name": POSTGRES_HOST,
+        "remove": True,
+        "environment": {
+            "POSTGRES_USER": POSTGRES_USER,
+            "POSTGRES_PASSWORD": POSTGRES_PASSWORD,
+        },
+    }
+
+    if IS_LOCAL:
+        options["ports"] = {"5432/tcp": f"{LOCAL_PORT}"}
+    else:
+        run_list = client.containers.list()
+        if len(run_list) > 0:
+            network = list(
+                run_list[0].attrs["NetworkSettings"]["Networks"].keys()
+            )[0]
+            options["network"] = network
+
     container = client.containers.run(
         "postgres:13.9-bullseye",
-        detach=True,
-        network=network,
-        name="tests_httpbin_1",
-        remove=True,
-        environment={
-            "POSTGRES_PASSWORD": "admin",
-        },
+        **options,
     )
 
     try:
         wait_until_responsive(lambda: check(container), 1.0)
-        # time.sleep(20)
         yield container
     except Exception as e:
         print(e)
@@ -50,10 +67,18 @@ def container():
 
 
 def test_status_code(container):
+    if IS_LOCAL:
+        host = "localhost"
+        port = LOCAL_PORT
+    else:
+        host = POSTGRES_HOST
+        port = "5432"
+    
     with psycopg2.connect(
-            host="tests_httpbin_1",
-            user="postgres",
-            password="admin",
+            host=host,
+            port=port,
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD,
     ) as db:
         with db.cursor() as cur:
             cur.execute("CREATE TABLE IF NOT EXISTS test (id TEXT PRIMARY KEY, name TEXT);")
